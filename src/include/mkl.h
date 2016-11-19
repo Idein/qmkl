e9fa7ff2dfefce8922f14d68f66b0e67e259922f
Terminus <i.can.speak.c.and.basic@gmail.com>
2016-11-20 02:50:04 +0900

    Add mkl.h for compability

---
+++ src/include/mkl.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2016 Sugizaki Yukimasa (ysugi@idein.jp)
+ * All rights reserved.
+ *
+ * This software is licensed under a Modified (3-Clause) BSD License.
+ * You should have received a copy of this license along with this
+ * software. If not, contact the copyright holder above.
+ */
+
+#ifndef _MKL_H_
+#define _MKL_H_
+
+#include "qmkl.h"
+
+#endif /* _MKL_H_ */
