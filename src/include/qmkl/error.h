00b683467602d5678b3b43d8a6f1e05acc011b68
Terminus <i.can.speak.c.and.basic@gmail.com>
2017-02-13 16:55:05 +0900

    Forget to add qmkl/error.h

---
+++ src/include/qmkl/error.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2017 Sugizaki Yukimasa at Idein Inc. (ysugi@idein.jp)
+ * All rights reserved.
+ *
+ * This software is licensed under a Modified (3-Clause) BSD License.
+ * You should have received a copy of this license along with this
+ * software. If not, contact the copyright holder above.
+ */
+
+#ifndef _QMKL_ERROR_H_
+#define _QMKL_ERROR_H_
+
+#endif /* _QMKL_ERROR_H_ */
